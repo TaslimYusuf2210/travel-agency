2a403921dad3d9c43468838cde86bebadedcb52d
Toy <112773318+TaslimYusuf2210@users.noreply.github.com>
2026-08-28 17:10:43 +0100

    feat(bookingdetails): add booking details page

---
+++ src/app/features/find-flight/pages/bookingdetails/bookingdetails.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookingdetails',
+  imports: [],
+  templateUrl: './bookingdetails.html',
+  styleUrl: './bookingdetails.css',
+})
+export class Bookingdetails {}
