f335f5a9e7db6cedb7281422e89838da6c87de87
Toy <112773318+TaslimYusuf2210@users.noreply.github.com>
2026-08-29 22:11:59 +0100

    refactor(find-flight): remove unused bookingdetails page

--- src/app/features/find-flight/pages/bookingdetails/bookingdetails.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-bookingdetails',
-  imports: [],
-  templateUrl: './bookingdetails.html',
-  styleUrl: './bookingdetails.css',
-})
-export class Bookingdetails {}
