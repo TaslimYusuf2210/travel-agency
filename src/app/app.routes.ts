ab2658b5bf50fd3208fd143048e949315be96b00
Toy <112773318+TaslimYusuf2210@users.noreply.github.com>
2026-08-29 22:13:08 +0100

    fix(routes): update imports for flightflow, flightsearch and flightlisting renames

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,50 +9,37 @@ export const routes: Routes = [
       {
         path: '',
         title: 'Travel Agency',
-        loadComponent: () => import('./features/home/home').then((m) => m.Home),
-      },
-      {
-        path: 'destinations',
-        title: 'Destinations',
         loadComponent: () =>
-          import('./features/destinations/destinations').then((m) => m.Destinations),
-      },
-      {
-        path: 'tours',
-        title: 'Tours',
-        loadComponent: () => import('./features/tours/tours').then((m) => m.Tours),
-      },
-      {
-        path: 'booking',
-        title: 'Booking',
-        loadComponent: () => import('./features/booking/booking').then((m) => m.Booking),
+          import('./features/home/home').then((m) => m.Home),
       },
       {
         path: 'find-flight',
-        title: 'Find Flight',
-        loadComponent: () =>
-          import('./features/find-flight/find-flight').then((m) => m.FindFlight),
-      },
-      {
-        path: 'showflights',
-        title: 'Show Flights',
-        loadComponent: () =>
-          import('./features/find-flight/pages/showflights/showflights').then((m) => m.Showflights),
+        loadChildren: () =>
+          import('./features/find-flight/find-flight.routes').then(
+            (m) => m.findFlightRoutes,
+          ),
       },
       {
         path: 'login',
         title: 'Login',
-        loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+        loadComponent: () =>
+          import('./features/auth/login/login').then((m) => m.Login),
       },
       {
         path: 'create-account',
         title: 'Create Account',
-        loadComponent: () => import('./features/auth/create-account/create-account').then((m) => m.CreateAccount),
+        loadComponent: () =>
+          import('./features/auth/create-account/create-account').then(
+            (m) => m.CreateAccount,
+          ),
       },
       {
         path: 'forgot-password',
         title: 'Forgot Password',
-        loadComponent: () => import('./features/auth/forget-password/forget-password').then((m) => m.ForgetPassword),
+        loadComponent: () =>
+          import('./features/auth/forget-password/forget-password').then(
+            (m) => m.ForgetPassword,
+          ),
       },
       { path: '**', redirectTo: '' },
     ],
